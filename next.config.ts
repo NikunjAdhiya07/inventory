c16101cbff3e3ac748283b4575040576bcdab96f
Nikunj Adhiya <vedantraval30@gmail.com>
2026-07-25 18:00:09 +0530

    fix: update allowed development origin for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ["192.168.0.132","https://inventory-theta-seven.vercel.app"]
+  allowedDevOrigins: ["192.168.0.132","https://inventory-indiana1.vercel.app"]
 };
 
 export default nextConfig;
